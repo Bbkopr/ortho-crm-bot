f87a8213e5e797a8919e5f9a101bd1efc26b7f1b
Bbkopr <126807433+Bbkopr@users.noreply.github.com>
2026-06-05 00:13:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
-bot.py
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "8921530434:AAFAq2jXi2X8Kqoc0QCfi1t9AFa3JIJecwI"
+TOKEN = os.getenv("8921530434:AAFAq2jXi2X8Kqoc0QCfi1t9AFa3JIJecwI")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 CRM مطب فعال شد")
+    await update.message.reply_text("🤖 ربات CRM مطب فعال شد")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text("📩 " + update.message.text)
 
-app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-app.run_polling()
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()

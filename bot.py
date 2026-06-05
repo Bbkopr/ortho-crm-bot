c09b08e6e57bd8630cf54f17835b8b1b66a5a229
Bbkopr <126807433+Bbkopr@users.noreply.github.com>
2026-06-05 11:14:04 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,6 @@
 import os
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
 
-TOKEN = os.environ.get("8921530434:AAFAq2jXi2X8Kqoc0QCfi1t9AFa3JIJecwI")
-
-print("TOKEN FOUND:", TOKEN is not None)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 ربات CRM مطب فعال شد")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
-
-def main():
-    if not TOKEN:
-        raise ValueError("TOKEN variable not found")
-
-    app = ApplicationBuilder().token(TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(
-        MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    )
-
-    print("Bot is running...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+print("===== ENV TEST =====")
+print("TOKEN EXISTS:", "TOKEN" in os.environ)
+print("TOKEN VALUE:", os.environ.get("TOKEN"))
+print("====================")
